c00f2768c014a3f32dd3554af26cb3b0032f84b8
ALFARO OLIVE Oscar <oscar.alfaro@eurocontrol.int>
2024-08-22 13:10:11 +0200

    SES aucu table

--- R/table_level2_cef_aucu.R
+++ R/table_level2_cef_aucu.R
@@ -14,46 +14,113 @@ mycz_name <- if_else(cztype == "terminal",
                      tcz_list$tcz_name[ez],
                      ecz_list$ecz_name[ez])
 
-# import data ----
-data_calc <- aucu(cztype, mycz)
+if (country == "SES RP3") {
+  # SES  ----
+  ## import data  ----
+  data_raw  <-  read_xlsx(
+    paste0(data_folder, "SES CEFF.xlsx"),
+    sheet = if_else(cztype == "terminal", "SES_TRM_all", "SES_ERT_all"),
+    range = cell_limits(c(1, 1), c(NA, NA))) %>%
+    as_tibble() %>% 
+    clean_names() 
   
-# prep data ----
-data_prep <- data_calc %>% 
-  filter(year_text == if_else(year_report == 2021 | year_report == 2020, "2020-2021", as.character(year_report))) %>% 
-  select(
-    initial_duc,
-    retro_ur,
-    new_duc,
-    infl_adj,
-    dif_a_d_costs,
-    trs_adj,
-    dc_notrs,
-    fin_inc,
-    rev_c_mod,
-    cross_fin,
-    other_rev,
-    loss_rev,
-    total_adjustments_aucu,
-    aucu
-  ) %>% 
-  rename(
-    'Initial DUC charged' = initial_duc,
-    'DUC to be charged retroactively' = retro_ur,
-    DUC = new_duc,
-    'Inflation adjustment' = infl_adj,
-    'Cost exempt from cost-sharing' = dif_a_d_costs,
-    'Traffic risk sharing adjustment' = trs_adj,
-    'Traffic adj. (costs not TRS)' = dc_notrs,
-    'Finantial incentives' = fin_inc,
-    'Modulation of charges' = rev_c_mod,
-    'Cross-financing' = cross_fin,
-    'Other revenues' = other_rev,
-    'Application of lower unit rate' = loss_rev,
-    'Total adjustments' = total_adjustments_aucu,
-    AUCU = aucu
-  ) %>% 
-  mutate('AUCU vs. DUC' = AUCU/DUC-1 ) %>% 
-  pivot_longer( cols = everything() ,names_to = "type", values_to = "value")
+    data_prep_split <- data_raw %>% 
+    filter(status == "A") |> 
+    mutate(year = as.character(year)) |> 
+    select(
+      year,
+      duc_initially_charged_eur_combined,
+      retroactive_application_of_unit_rate_combined,
+      new_duc_eur_combined,
+      inflation_adjustment_eur_su,
+      cecs_total_eur_su,
+      trs_eur_su,
+      traffic_adjustment_eur_su,
+      incentives_eur_su,
+      modulation_eur_su,
+      cross_financing_eur_su,
+      other_revenues_eur_su_combined,
+      unit_rate_reduction_su,
+      total_adjustments_su_combined,
+      aucu_combined
+      ) |> 
+      mutate('AUCU vs. DUC' = aucu_combined/new_duc_eur_combined-1 ) |> 
+      pivot_longer(-c(year), names_to = "type", values_to = "value")
+  
+  data_prep2020_2021 <- data_prep_split %>% 
+    filter(
+      year < 2022) %>% 
+    group_by(type) |> 
+    summarise(value = sum(value, na.rm = TRUE)) |> 
+    mutate(year = "2020-2021") 
+  
+  data_prep <- data_prep_split |> 
+    rbind(data_prep2020_2021) |> 
+    filter(year == if_else(year_report == 2020 | year_report == 2021, "2020-2021", as.character(year_report))) |> 
+    select(-year) |> 
+    mutate(value = round(value, 2),
+           type = c('Initial DUC charged',
+                      'DUC to be charged retroactively',
+                      'DUC',
+                      'Inflation adjustment',
+                      'Cost exempt from cost-sharing',
+                      'Traffic risk sharing adjustment',
+                      'Traffic adj. (costs not TRS)',
+                      'Finantial incentives',
+                      'Modulation of charges',
+                      'Cross-financing',
+                      'Other revenues',
+                      'Application of lower unit rate',
+                      'Total adjustments',
+                      'AUCU',
+                      'AUCU vs. DUC')
+    ) 
+  
+  
+} else {
+  # State  ----
+  ## import data  ----
+  data_calc <- aucu(cztype, mycz)
+  
+  ## prep data ----
+  data_prep <- data_calc %>% 
+    filter(year_text == if_else(year_report == 2021 | year_report == 2020, "2020-2021", as.character(year_report))) %>% 
+    select(
+      initial_duc,
+      retro_ur,
+      new_duc,
+      infl_adj,
+      dif_a_d_costs,
+      trs_adj,
+      dc_notrs,
+      fin_inc,
+      rev_c_mod,
+      cross_fin,
+      other_rev,
+      loss_rev,
+      total_adjustments_aucu,
+      aucu
+    ) %>% 
+    rename(
+      'Initial DUC charged' = initial_duc,
+      'DUC to be charged retroactively' = retro_ur,
+      DUC = new_duc,
+      'Inflation adjustment' = infl_adj,
+      'Cost exempt from cost-sharing' = dif_a_d_costs,
+      'Traffic risk sharing adjustment' = trs_adj,
+      'Traffic adj. (costs not TRS)' = dc_notrs,
+      'Finantial incentives' = fin_inc,
+      'Modulation of charges' = rev_c_mod,
+      'Cross-financing' = cross_fin,
+      'Other revenues' = other_rev,
+      'Application of lower unit rate' = loss_rev,
+      'Total adjustments' = total_adjustments_aucu,
+      AUCU = aucu
+    ) %>% 
+    mutate('AUCU vs. DUC' = AUCU/DUC-1 ) %>% 
+    pivot_longer(cols = everything() ,names_to = "type", values_to = "value")
+}
+  
 
 # plot chart  ----
 mygtable(data_prep, myfont*0.95) %>% 
