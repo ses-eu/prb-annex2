5d0b7e9e29a4f5722f2d55fddb7496538e7002d6
ALFARO OLIVE Oscar <oscar.alfaro@eurocontrol.int>
2025-04-05 15:15:43 +0200

    some new investment tables

--- R/chart_invest_av_type.R
+++ R/chart_invest_av_type.R
@@ -1,71 +1,99 @@
-if (exists("country") == FALSE) {country <- "Bulgaria"}
+if (exists("country") == FALSE) {country <- "France"}
 
 source("R/parameters.R")
 
 # import data  ----
-if (!exists("data_category")) {
+if (!exists("data_capex")) {
   source("R/get_investment_data.R")
 }
 
-
 # process data  ----
-data_prep <- data_category %>% 
-  filter(member_state_1 == .env$country) %>% 
-  select(atm, cns, infra, ancillary, unknown, other) %>% 
-  summarise (atm = sum(atm, na.rm=TRUE),
-             cns = sum(cns, na.rm=TRUE), 
-             infra = sum(infra, na.rm=TRUE), 
-             unknown = sum(unknown, na.rm=TRUE),
-             other = sum(other, na.rm=TRUE) + sum(ancillary, na.rm=TRUE)) %>% 
-  mutate(total = rowSums(across(everything())),
-         atm_share = atm/total * 100,
-         cns_share = cns/total * 100,
-         infra_share = infra/total * 100,
-         unknown_share = unknown/total *100,
-         other_share = other/total*100)%>% 
-  select(atm_share,
-         cns_share,
-         infra_share,
-         unknown_share,
-         other_share) %>% 
+data_prep <- data_capex %>% 
+  filter(member_state == .env$country) %>% 
+  mutate(
+    pp_new_major_share = new_major_investments_as_per_pp / total * 100,
+    pp_other_new_share = other_new_investments_as_per_pp / total * 100,
+    add_new_major_share = additional_new_major_investments / total * 100,
+    add_other_new_share = 0
+  ) %>% 
+  select(
+    pp_new_major_share,
+    pp_other_new_share,
+    add_new_major_share,
+    add_other_new_share
+  ) %>% 
   gather() %>% 
+  mutate(category = ifelse(grepl("^pp", key), "pp", "add"),  # Extract category
+         share_type = sub("^(pp_|add_)", "", key)) %>%  # Remove category prefix
   mutate(
     type = case_when(
-      key == "atm_share" ~ "ATM systems",
-      key == "cns_share" ~ "CNS systems",
-      key == "infra_share" ~ "Infrastructure",
-      key == "unknown_share" ~ "Unknown",
-      key == "other_share" ~ "Other"
+      share_type == "new_major_share" ~ "New major investments",
+      share_type == "other_new_share" ~ "Other new investments"
     ),
-    xlabel = "ANSP",
-    mymetric = round(value,0),
-    textposition = if_else(mymetric == 0 | mymetric > 2, "inside", "outside"),
-    textlabel = if_else(mymetric == 0, " ", paste0(format(round(mymetric,0), nsmall = 0), "%"))
-  )  %>% 
-  select(type, mymetric, textlabel, textposition) 
-
+    xlabel = case_when(
+      category == "pp" ~ "Included in\nthe performance plan",
+      category == "add" ~ "Additional"
+    ),
+    xlabel = factor(xlabel, levels = c("Included in\nthe performance plan", "Additional")),
+    mymetric = value
+  ) %>% 
+  arrange(desc(xlabel)) %>% 
+  select(xlabel, type, mymetric) 
 
 # chart ----
-## legend
+## chart parameters ----
+local_suffix <- "%"
+local_mydecimals <- 0
+
+###set up order of traces
+local_hovertemplate <- paste0('%{y:,.', local_mydecimals, 'f}', local_suffix)
+
+#### legend
 if (knitr::is_latex_output()) {
-  local_legend_x <- 1
-  local_legend_y <- 0.5  
+  local_legend_y <- mylegend_y
+  local_legend_x <- -0.18
+  local_legend_xanchor <- 'left'
+  local_legend_fontsize <- myfont-1
+  
 } else {
+  local_legend_y <- -0.2
   local_legend_x <- 0.5
-  local_legend_y <- -0.05
   local_legend_xanchor <- 'center'
+  local_legend_fontsize <- myfont
+  
 }
 
-
-
 # plot chart ----
-mydonutchart(data_prep, 
-             colors = c( '#044598', '#22A0DD', '#58595B', '#FFF000', '#7030A0'),
-             hovertemplate = "%{label}: %{value}%<extra></extra>",
-             title_text = "Asset value by type of investment",
-             minsize = 14,
-             legend_x = local_legend_x,
-             legend_y = local_legend_y,
-             legend_xanchor = local_legend_xanchor,
-             legend_orientation = "h")
+myplot <- mybarchart2(data_prep, 
+                      height = myheight + 20,
+                      colors = c('#044598', '#22A0DD'),
+                      local_factor = c("Other new investments", "New major investments"),
+                      
+                      suffix = local_suffix,
+                      decimals = local_mydecimals,
+                      
+                      hovertemplate = local_hovertemplate,
+                      hovermode = "x unified",
+                      
+                      textangle = 0,
+                      textposition = "inside",
+                      insidetextanchor = 'middle',
+                      textfont_color = 'white',
 
+                      bargap = 0.25,
+                      barmode = 'stack',
+                      
+                      title_text = "Asset value by investment category",
+                      title_y = 0.99,
+                      
+                      yaxis_title = "Total new asset value (%)",
+                      yaxis_ticksuffix = "%",
+                      yaxis_tickformat = ".0f",
+                      
+                      legend_y = local_legend_y, 
+                      legend_x = local_legend_x,
+                      legend_xanchor = local_legend_xanchor,
+                      legend_fontsize = local_legend_fontsize,
+                      margin = list (t = 40, b= 60))
+
+myplot
